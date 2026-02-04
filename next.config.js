814db65fa9d0134e54c1fc940bb99efdfe8862a9
zackwek <zkwe0001@student.monash.edu>
2026-02-05 01:05:31 +1100

    Issue is because of conflicting deployment instrucitons, deploy based of YAML and not github deploy frombranches feature

--- next.config.js
+++ next.config.js
@@ -8,3 +8,6 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+/**Test change 4: website was crashing as github was deploying change from branch (deploying old stock file ) while Yaml was triggering the 
+ * latest up to date deployment files 
+ */
